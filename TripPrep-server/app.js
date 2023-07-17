914e718c08f860bdf1067a240346c67e491d2aa7
robopecha <julikolb@gmail.com>
2023-07-17 18:21:59 +0200

    adding routes to server

--- TripPrep-server/app.js
+++ TripPrep-server/app.js
@@ -1,24 +1,24 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv").config();
-
-// ℹ️ Connects to the database
+require("dotenv/config");
 require("./db");
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+const allRoutes = require("./routes");
+app.use("/api", allRoutes);
 
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+const tripRouter = require("./routes/trip.routes");
+app.use("/api", tripRouter);
+
+const listRouter = require("./routes/list.routes");
+app.use("/api", listRouter);
+
+const itemRouter = require("./routes/item.routes");
+app.use("/api", itemRouter);
+
 require("./error-handling")(app);
 
 module.exports = app;
