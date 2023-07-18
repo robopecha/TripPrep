9d42006456869b9c0ed2860a570b71cc1deb144b
robopecha <julikolb@gmail.com>
2023-07-18 22:40:24 +0200

    added client setup files and basic auth

--- TripPrep-server/models/Item.model.js
+++ TripPrep-server/models/Item.model.js
@@ -4,7 +4,8 @@ const { Schema, model } = mongoose;
 const itemSchema = new Schema({
   content: { type: String },
   list: { type: Schema.Types.ObjectId, ref: "List" },
-  public: { type: Boolean, default: true }
+  public: { type: Boolean, default: true },
+  packed: { type: Boolean, default: false }
 });
 
 module.exports = model("Item", itemSchema);
