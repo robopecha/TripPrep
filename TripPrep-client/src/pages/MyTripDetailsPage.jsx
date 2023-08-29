c0c8ee0ba80be8f374490721b71aa70ad85dadb1
robopecha <julikolb@gmail.com>
2023-08-29 15:47:50 +0200

    datedisplaying

--- TripPrep-client/src/pages/MyTripDetailsPage.jsx
+++ TripPrep-client/src/pages/MyTripDetailsPage.jsx
@@ -7,19 +7,21 @@ function MyTripDetailsPage() {
   const {trips} = useContext(TripContext);
   const {tripID} = useParams();
 
-  const theTrip = trips.filter(trip => trip._id === tripID)
+  const theTrip = trips.filter(trip => trip._id === tripID);
+  console.log('this is it: ', theTrip);
 
   return (
     <>
-     <h3 className="text-4xl my-6 text-center mt-15">{theTrip[0]?.destination}</h3>
+     <h3 className="text-4xl mt-10 mb-6 text-center mt-15">{theTrip[0]?.destination}</h3>
      <h3 className="text-2xl my-6 text-center">{theTrip[0]?.country}</h3>
      <ListCard {...theTrip[0]}/>
      <h3 className="text-2xl my-6 text-center">{theTrip[0]?.season}</h3>
-     <h3 className="text-2xl mt-10 text-center">{theTrip[0]?.startDate}</h3>
+     <h3 className="text-xl mt-10 text-center">starting on:</h3>
+     <h3 className="text-2xl mt-3 text-center">{theTrip[0]?.startDate.slice(8, 10)}-{theTrip[0]?.startDate.slice(5, 7)}-{theTrip[0]?.startDate.slice(0, 4)}</h3>
     </>
   );
 }
 
-// add edit option and image upload option
+// add edit option
 
 export default MyTripDetailsPage;
--- TripPrep-server/models/Trip.model.js
+++ TripPrep-server/models/Trip.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
 const tripSchema = new Schema({
-  destination: { type: String },
+  destination: { type: String, required: true },
   country: { type: String, required: true },
   season: { type: String, required: true },
-  startDate: { type: Date },
+  startDate: { type: Date, required: true },
   lists: [{ type: Schema.Types.ObjectId, ref: "List" }],
   packed: { type: Boolean, default: false },
   public: { type: Boolean, default: false },
