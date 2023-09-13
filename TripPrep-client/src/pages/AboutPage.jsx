fa8ed210722437dc2f60bfed28a42e24f96dd1f9
robopecha <julikolb@gmail.com>
2023-09-13 20:36:29 +0200

    added about page

---
+++ TripPrep-client/src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import { useParams, Link } from "react-router-dom";
+
+function AboutPage() {
+
+  return (
+    <>
+      <h3 className="text-4xl my-6">About</h3>
+      <span>
+        bla bla bla
+      </span>
+    </>
+  );
+}
+
+export default AboutPage;
