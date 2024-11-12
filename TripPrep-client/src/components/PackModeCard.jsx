184ed3ae2a9c089c2584bc9c87e137c46fa6ab25
robopecha <julikolb@gmail.com>
2024-11-12 19:15:36 +0100

    moved done

--- TripPrep-client/src/components/PackModeCard.jsx
+++ TripPrep-client/src/components/PackModeCard.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import { mutate } from "swr";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
 
 
-function PackModeCard({ item, onClick }) {
+function PackModeCard({ item, successCheck }) {
+
+  const [itemDone, setItemDone] = React.useState(item.done);
+
+  function toggleClick() {
+    setItemDone(!itemDone);
+  }
+
+  React.useEffect(() => {
+    const storedToken = localStorage.getItem('authToken');
+    const requestBody = { done: itemDone };
+    axios
+    .put(
+      `${API_URL}/api/items/${item._id}`,
+      requestBody,
+      { headers: { Authorization: `Bearer ${storedToken}` } }
+    )
+    .then(() => {
+      mutate(`${API_URL}/api/items`);
+      successCheck();
+      })
+      .catch((error) => console.log(error))
+
+  }, [itemDone]);
 
   return (
     <div className={item.done ? "border-2 border-black rounded-sm mb-4 p-3 overflow-scroll bg-green-400 hover:bg-green-500 w-80"
-      : "border-2 border-black rounded-sm mb-4 p-3 overflow-scroll bg-white hover:bg-gray-100 w-80"} onClick={onClick}>
+      : "border-2 border-black rounded-sm mb-4 p-3 overflow-scroll bg-white hover:bg-gray-100 w-80"} onClick={toggleClick}>
       <h5 className="text-lg">{item.content}</h5>
     </div>
   );
--- TripPrep-client/src/pages/PackModePage.jsx
+++ TripPrep-client/src/pages/PackModePage.jsx
@@ -2,10 +2,10 @@ import PackModeCard from "../components/PackModeCard";
 import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import ItemContext from "../context/item.context"
-import { mutate } from "swr";
-import axios from "axios";
+// import { mutate } from "swr";
+// import axios from "axios";
 
-const API_URL = "http://localhost:5005";
+// const API_URL = "http://localhost:5005";
 
 
 const listType = "topack";
@@ -18,32 +18,12 @@ function PackModePage() {
   const { tripID } = useParams();
   const theItems = items?.filter((item) => item.trip === tripID && item.listType === listType);
 
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  function handleItemClick(itemId) {
-    const clickedItem = theItems.find((item) => item._id === itemId);
-
-    if (clickedItem) {
-      const toggledStatus = !clickedItem.done;
-
-      const storedToken = localStorage.getItem('authToken');
-      const requestBody = { done: toggledStatus };
-      axios
-        .put(
-          `${API_URL}/api/items/${itemId}`,
-          requestBody,
-          { headers: { Authorization: `Bearer ${storedToken}` } }
-        )
-        .then(() => {
-          mutate(`${API_URL}/api/items`);
-          const allDone = theItems.every(item => item._id === itemId ? toggledStatus : item.done);
-          setSuccess(allDone);
-        })
-        .catch((error) => console.log(error));
-    } else {
-      console.error("Item not found");
-    }
-    console.log('success:', success);
+  function successCheck() {
+    const allDone = theItems.every(item => item.done);
+    console.log('all done:', allDone);
+    setSuccess(allDone);
   }
 
   return (
@@ -51,7 +31,7 @@ function PackModePage() {
       <h3 className="text-4xl my-8">Packing!</h3>
       {isLoading && <p>Loading list...</p>}
       {error && <p>Failed to load list.</p>}
-      {theItems?.map((item) => <PackModeCard key={item._id} item={item} onClick={() => handleItemClick(item._id)} />)}
+      {theItems.map((item) => <PackModeCard key={item._id} item={item} successCheck={successCheck} />)}
     </div>
   );
 }
@@ -63,5 +43,3 @@ export default PackModePage;
 // add success state that enables navigate to success page
 
 // navigate(`/trips/${tripID}/lists/success`);
-
-//return theItems.every(item => item.done);
