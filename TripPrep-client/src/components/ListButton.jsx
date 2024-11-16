877c8d40ce84c69cd028882cc78ef7333050157c
robopecha <julikolb@gmail.com>
2024-11-16 10:12:58 +0100

    list button

--- TripPrep-client/src/components/ListButton.jsx
+++ TripPrep-client/src/components/ListButton.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 
-function ListButton({ to, className = '', children }) {
+function ListButton({ to, done, children }) {
 
   return (
-    <Link to={to}>
-      <button className={`rounded-sm border-2 border-white hover:border-black transition ease-in-out duration-200 bg-yellow-400 text-center w-60 py-2 my-2 text-xl ${className}`}>
-        {children}
-      </button>
-    </Link>
+    <div className="relative inline-block">
+      <Link to={to}>
+        <button className="relative rounded-sm border-2 border-white hover:border-black transition ease-in-out duration-200 bg-yellow-400 text-center w-60 py-2 my-2 text-xl">
+          {children}
+        </button>
+      </Link>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor"
+        className={done ? 'absolute -top-8 left-48 w-24 visible' : 'absolute -top-8 left-48 w-24 invisible'}
+      >
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+      </svg>
+    </div>
   )
 }
 
--- TripPrep-client/src/components/ListsCard.jsx
+++ TripPrep-client/src/components/ListsCard.jsx
@@ -6,17 +6,10 @@ import ListButton from "./ListButton";
 function ListsCard({ theTrip }) {
 
   return (
-    <div className="my-10 flex flex-col items-center">
-      <ListButton to={`/trips/${theTrip?._id}/lists/todo`} className='mb-8'>To Do</ListButton>
-      <ListButton to={`/trips/${theTrip?._id}/lists/tobuy`} className='my-10'>To Buy</ListButton>
-      <div className="relative inline-block">
-        <ListButton to={`/trips/${theTrip?._id}/lists/topack`} className='relative mt-8'>To Pack</ListButton>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="currentColor"
-          className={theTrip?.packedDone ? 'absolute -top-2 left-48 w-24 visible' : 'absolute -top-2 left-48 w-24 invisible'}
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-        </svg>
-      </div>
+    <div className="my-10 flex flex-col items-center gap-6">
+      <ListButton to={`/trips/${theTrip?._id}/lists/todo`} done={theTrip?.doDone}>To Do</ListButton>
+      <ListButton to={`/trips/${theTrip?._id}/lists/tobuy`} done={theTrip?.buyDone}>To Buy</ListButton>
+      <ListButton to={`/trips/${theTrip?._id}/lists/topack`} done={theTrip?.packDone}>To Pack</ListButton>
     </div>
   );
 }
