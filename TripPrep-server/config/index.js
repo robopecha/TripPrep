5971acbd17f3447079ae891341d25fd13163aee4
robopecha <julikolb@gmail.com>
2024-11-27 00:40:43 +0100

    deployment fixes

--- TripPrep-server/config/index.js
+++ TripPrep-server/config/index.js
@@ -8,7 +8,7 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://tripprep.netlify.app/"],
+      origin: ["http://localhost:5173", "http://127.0.0.1:5173", "https://tripprep.netlify.app"],
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       allowedHeaders: ['Content-Type', 'Authorization']
