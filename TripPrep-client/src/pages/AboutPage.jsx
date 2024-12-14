16ecf7f57609fec3a74a7fcc7e5a33a37b46ef8f
robopecha <julikolb@gmail.com>
2024-12-14 15:54:59 +0100

    colors

--- TripPrep-client/src/pages/AboutPage.jsx
+++ TripPrep-client/src/pages/AboutPage.jsx
@@ -10,11 +10,11 @@ function AboutPage() {
     <>
       <Header>About TripPrep</Header>
       <p className='mb-12'>
-        TripPrep is an app based on my personal travel prep system,<br />
+        TripPrep is an app based on my personal pen & paper travel prep system,<br />
         that allows me to keep track of everything that happens before my trip.<br />
         This way I can leave the house relaxed, knowing that everything is done and I forgot nothing.
       </p>
-      <p className='mb-12'>
+      <p className='mb-12 text-yellow-500'>
         I am using three sheets of paper:<br />
         One for everything that needs to be done before the trip,<br />
         one for everything I need to buy,<br />
@@ -26,7 +26,7 @@ function AboutPage() {
         A packing mode that allows you to simply check off the packing list items while packing.<br />
         And the option to share your packing list with other users and to look at others' packing lists for inspiration.
       </p>
-      <p className='mb-12'>
+      <p className='mb-12 text-red-500'>
         Your list will only be shared publicly after you mark it as "Everything is packed!" and set your trip to public.<br />
         Only your To Pack list will be seen by others. Nobody will see your To Do and To Buy lists.
       </p>
