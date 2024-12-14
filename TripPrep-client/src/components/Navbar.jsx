81c637c62cf602bef9275701024632ed4fc452dc
robopecha <julikolb@gmail.com>
2024-12-14 15:45:35 +0100

    height

--- TripPrep-client/src/components/Navbar.jsx
+++ TripPrep-client/src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
 
   return (
     <>
-      <nav className="fixed top-0 left-0 w-full flex justify-between items-center bg-red-500 text-lg py-3 z-50">
+      <nav className="fixed top-0 left-0 w-full flex justify-between items-center bg-red-500 text-lg h-16 z-50">
 
           <NavLink to="/">
             <div className="flex flex-row items-center">
@@ -56,7 +56,7 @@ function Navbar() {
       </nav>
 
 
-      <nav className="md:hidden fixed bottom-0 left-0 w-full flex justify-around items-center bg-red-500 text-3xl py-4 z-50">
+      <nav className="md:hidden fixed bottom-0 left-0 w-full flex justify-around items-center bg-red-500 text-3xl h-16 z-50">
 
         <NavLink to="/" className={({ isActive }) => isActive ? "text-white" : "hover:text-yellow-300 transition ease-in-out duration-100"}>
           <AiOutlineHome />
