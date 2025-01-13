444840b251f233c465a37ec397ba0c5b2559b98b
robopecha <julikolb@gmail.com>
2025-01-13 14:37:58 +0100

    prop delegation

--- TripPrep-client/src/components/BlueButton.jsx
+++ TripPrep-client/src/components/BlueButton.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
 
-function BlueButton({ type, className = '', onClick, children, ...delegated }) {
+function BlueButton({ className = '', children, ...delegated }) {
 
   return (
     <button
-      type={type}
-      className={`bg-blue-500 p-2 border-2 border-white rounded-sm hover:border-black transition ease-in-out duration-200 ${className}`}
-      onClick={onClick}
       {...delegated}
+      className={`bg-blue-500 p-2 border-2 border-white rounded-sm hover:border-black transition ease-in-out duration-200 ${className}`}
     >
       {children}
     </button>
