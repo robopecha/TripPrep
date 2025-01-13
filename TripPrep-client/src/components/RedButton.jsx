8023d7f99f2af70cd7a43ab1cd1b90aaea7eb8f1
robopecha <julikolb@gmail.com>
2025-01-13 16:46:20 +0100

    prop delegation

--- TripPrep-client/src/components/RedButton.jsx
+++ TripPrep-client/src/components/RedButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 
-function RedButton({ className = '', onClick, children }) {
+function RedButton({ className = '', children, ...delegated }) {
 
   return(
     <button
+      {...delegated}
       className={`p-2 text-red-500 bg-white border-2 rounded-sm border-red-500 hover:bg-red-500 hover:text-black hover:border-black transition ease-in-out duration-200 ${className}`}
-      onClick={onClick}
     >
       {children}
     </button>
