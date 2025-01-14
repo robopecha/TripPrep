d349a0d0ff0d457eb7cd3a25c514735a276da2e4
robopecha <julikolb@gmail.com>
2025-01-15 00:13:24 +0100

    h1

--- TripPrep-client/src/components/Header.jsx
+++ TripPrep-client/src/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Header({ className = '', children }) {
 
   return (
-    <h3 className={`text-4xl my-7 ${className}`}>{ children }</h3>
+    <h1 className={`text-4xl my-7 ${className}`}>{ children }</h1>
   )
 }
 
